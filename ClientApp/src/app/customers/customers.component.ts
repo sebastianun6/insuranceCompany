d32ab798cbab1a5e3174653f4e6ea52ffa1e1800
sebas <sebastianun@gmail.com>
2020-08-16 23:33:24 -0600

    implementation of the GetCustomers functionality in the front end. Also adding a link to use the Material Icons

--- ClientApp/src/app/customers/customers.component.ts
+++ ClientApp/src/app/customers/customers.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ICustomer } from '../interfaces';
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-
-  constructor() { }
+  customers: ICustomer[];
+  constructor(private dataService: DataService,) { }
 
   ngOnInit() {
+    this.dataService.getCustomers().subscribe((response: ICustomer[]) => {
+      this.customers = response;
+    },
+    (err: any) => console.log(err));
   }
 
 }
