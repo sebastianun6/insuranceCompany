5141426eb24115ab7165d7ec29feaf50f22e9a3a
sebas <sebastianun@gmail.com>
2020-08-17 23:30:05 -0600

    clean up, removing unused components.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CustomersComponent } from './customers/customers.component';
 import { PoliciesComponent } from './policies/policies.component';
 import { CustomersEditComponent } from './customers-edit/customers-edit.component';
@@ -19,8 +17,6 @@ import { PoliciesEditComponent } from './policies-edit/policies-edit.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     CustomersComponent,
     PoliciesComponent,
     CustomersEditComponent,
@@ -33,8 +29,6 @@ import { PoliciesEditComponent } from './policies-edit/policies-edit.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'customers', component: CustomersComponent },
       { path: 'customers/:id', component: CustomersEditComponent },
       { path: 'policies', component: PoliciesComponent },
